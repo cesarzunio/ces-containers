86a98a11543435be3017a24ed2296540799b8b83
cesarzunio <andrzejmarekpatalon@gmail.com>
2025-02-27 16:23:24 +0100

    RawPtr alloc fix

--- Raw/RawPtr.cs
+++ Raw/RawPtr.cs
@@ -19,24 +19,50 @@ namespace Ces.Collections
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public RawPtr(Allocator allocator, int alignment)
         {
+            if (allocator == Allocator.None)
+            {
+                Ptr = null;
+                _allocator = Allocator.None;
+                return;
+            }
+
             Ptr = CesMemoryUtility.Allocate<T>(1, alignment, allocator);
             _allocator = allocator;
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public RawPtr(Allocator allocator, int alignment, T value) : this(allocator, alignment)
+        public RawPtr(Allocator allocator, int alignment, T value)
         {
+            if (allocator == Allocator.None)
+            {
+                Ptr = null;
+                _allocator = Allocator.None;
+                return;
+            }
+
+            Ptr = CesMemoryUtility.Allocate<T>(1, alignment, allocator);
             *Ptr = value;
+            _allocator = allocator;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static RawPtr<T> Null()
+        {
+            return new(Allocator.None, 0);
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public void Dispose()
         {
             if (!IsCreated)
-                throw new Exception("RawPtr :: Dispose :: Is already disposed!");
+            {
+#if CES_COLLECTIONS_WARNING
+                Debug.LogError("RawPtr :: Dispose :: Is not created!");
+#endif
+                return;
+            }
 
-            UnsafeUtility.Free(Ptr, _allocator);
-            Ptr = null;
+            CesMemoryUtility.FreeAndNullify(ref Ptr, _allocator);
         }
 
         public readonly ref T Ref
@@ -46,10 +72,10 @@ namespace Ces.Collections
             {
 #if CES_COLLECTIONS_CHECK
                 if (!IsCreated)
-                    throw new Exception("RawPtr :: Ref :: Ptr is null!");
+                    throw new Exception("RawPtr :: Ref :: Is not created!");
 #endif
 
-                return ref (*Ptr);
+                return ref *Ptr;
             }
         }
     }
